953c41dc0bf4f467fbf98a7b2228ee66b249a60b
Oscar Padilla <72768285+Oscar-Padilla@users.noreply.github.com>
2024-07-22 16:47:41 -0600

    Cosillas 11

--- isomorphic/apps/isomorphic-i18n/src/app/api/auth/[...nextauth]/pages-options.ts
+++
@@ -1,7 +0,0 @@
-import { routes } from '@/config/routes';
-import { PagesOptions } from 'next-auth';
-
-export const pagesOptions: Partial<PagesOptions> = {
-  signIn: routes.signIn,
-  error: routes.signIn,
-};
--- isomorphic/apps/isomorphic-i18n/src/app/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Inter, Lexend_Deca } from 'next/font/google';
-
-export const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
-
-export const lexendDeca = Lexend_Deca({
-  subsets: ['latin'],
-  variable: '--font-lexend',
-});
